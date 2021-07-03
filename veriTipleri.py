750271cff197c48ea46c06fb592b7a7d29a72131
kamilsmr <kamils6035@gmail.com>
2021-07-03 16:50:13 +0300

    uptade

--- veriTipleri.py
+++ veriTipleri.py
@@ -17,3 +17,24 @@ print(b)
 print(c)
 print(d)
 print(e)
+
+x = 10
+print(x)
+x = 15
+print(x)
+
+a = 3.4
+print(a)
+b = 4.2
+print(b)
+
+a = 2
+b = 3
+c = (a+2) * b
+print(c)
+
+# 4 kural değişken isimleri
+# 1. değişken isimleri sayı ie başlayamaz
+# 2. değişken issmleri kelimelerden oluşuyorsa aralarında boşluk olamaz
+# 3. semboller değişken ismi içinde kullanılamaz sadece _ kullanılır
+# 4. tanımlı anahtar kelimeler değişken ismi olarak kullanılamazlar. örnek while do if vb..
