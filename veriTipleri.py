08d4eacead166284933afa0dd1f5dc1f632ae5fd
kamilsmr <kamils6035@gmail.com>
2021-07-03 16:42:47 +0300

    işlemler

---
+++ veriTipleri.py
@@ -0,0 +1,19 @@
+x = 3+4
+y = 5-2
+z = 3*4
+a = 28 / 7
+b = 2.1 + 3.2
+c = 3.2 * 5.4
+d = 45.6 / 6.3
+e = 2 + 3 - 1 * 9 
+
+
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
