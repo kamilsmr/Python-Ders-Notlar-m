c94ef7d85c00459f77fe3d211947d273daebc321
kamilsmr <kamils6035@gmail.com>
2021-07-06 22:54:55 +0300

    uptade

---
+++ strings.py
@@ -0,0 +1,37 @@
+
+# stringler
+# eğer tek veya çift tırnak ile nasıl başlarsa öyle bitmesi gerekir.
+
+# print('kamil')
+# print("kamil")
+# print("""kamil """)
+# kullanım yanlış
+# print("kamil')
+# iki tarz yazımı var kesmeli ifadenin
+# print("kamil'in kalemi var")
+# print('kamil\'in kalemi var')
+
+# string indexleri
+
+# a = "kamil"
+# print(a[0])
+# print(a[3])
+# print(a[4])
+# print(a[1])
+
+# b = "selim"
+# print(b[-1])
+# print(b[-4])
+# print(b[-2])
+# print(b[-3])
+
+# string parçalama 
+
+a = "Bir gun cok degerlidir."
+# print(a[3:10])
+# print(a[:5])
+# print(a[6:])
+# print(a[:])
+# print(a[:-1])
+# print(a[::2])
+print(a[3:18:2])
