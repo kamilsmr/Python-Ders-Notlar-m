6049393ad5c27f9c6bb798909d2db66dbf663719
kamilsmr <kamils6035@gmail.com>
2021-07-06 21:57:42 +0300

    continue

--- veriTipleri.py
+++ veriTipleri.py
@@ -1,53 +1,72 @@
-x = 3+4
-y = 5-2
-z = 3*4
-a = 28 / 7
-b = 2.1 + 3.2
-c = 3.2 * 5.4
-d = 45.6 / 6.3
-e = 2 + 3 - 1 * 9 
+# x = 3+4
+# y = 5-2
+# z = 3*4
+# a = 28 / 7
+# b = 2.1 + 3.2
+# c = 3.2 * 5.4
+# d = 45.6 / 6.3
+# e = 2 + 3 - 1 * 9 
 
-print(x)
-print(y)
-print(z)
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
+# print(x)
+# print(y)
+# print(z)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
 
-x = 10
-print(x)
-x = 15
-print(x)
+# x = 10
+# print(x)
+# x = 15
+# print(x)
 
-a = 3.4
-print(a)
-b = 4.2
-print(b)
+# a = 3.4
+# print(a)
+# b = 4.2
+# print(b)
 
-a = 2
-b = 3
-c = (a+2) * b
-print(c)
+# a = 2
+# b = 3
+# c = (a+2) * b
+# print(c)
 
-# 4 kural değişken isimleri
-# 1. değişken isimleri sayı ie başlayamaz
-# 2. değişken issmleri kelimelerden oluşuyorsa aralarında boşluk olamaz
-# 3. semboller değişken ismi içinde kullanılamaz sadece _ kullanılır
-# 4. tanımlı anahtar kelimeler değişken ismi olarak kullanılamazlar. örnek while do if vb..
+# # 4 kural değişken isimleri
+# # 1. değişken isimleri sayı ie başlayamaz
+# # 2. değişken issmleri kelimelerden oluşuyorsa aralarında boşluk olamaz
+# # 3. semboller değişken ismi içinde kullanılamaz sadece _ kullanılır
+# # 4. tanımlı anahtar kelimeler değişken ismi olarak kullanılamazlar. örnek while do if vb..
 
-#dairenin çevresi
-pi_sayısı = 3.14
-çap = 4
-çevre = pi_sayısı * çap
-print (çevre)
+# #dairenin çevresi
+# pi_sayısı = 3.14
+# çap = 4
+# çevre = pi_sayısı * çap
+# print (çevre)
 
-# değerleri değiştirme
-a =3
-b =4
-a,b = b,a
-print(a)
-print(b)
+# # değerleri değiştirme
+# a =3
+# b =4
+# a,b = b,a
+# print(a)
+# print(b)
+
+
+# i = 6
+# i = i + 1
+# print(i)
+
+# i = 8
+
+# i+=2
+# print(i)
+
+# x = 4
+# x = x*3
+# print(x)
+
+# x = 5
+# x*=6
+# print(x)
+
 
 
